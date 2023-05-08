2eed71fb901755c8c8cebc11f482b3cbfbdb57fa
Evgenia <evgeniayurkova@gmail.com>
2023-05-08 12:12:31 +0300

    add dropdown menu & styles for mobiles

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,29 @@
 import './categories.css';
 import Category from '../Category/Category';
+import { useState } from 'react';
 
 const Categories = () => {
   const categories = ['r/puppies', 'r/flowers', 'r/kittens', 'r/books', 'r/vacation'];
+  const [menuClicked, setMenuClicked] = useState(false);
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    menuClicked ? setMenuClicked(false) : setMenuClicked(true);
+  }
+
+  let dropdownMenu = !menuClicked ? {
+    display: 'none'
+  } : {
+    display: 'block'
+  };
+
+  let dropdownSymbol = !menuClicked ? '>' : 'x';
+
 
   return (
-    <div className='categories'>
+    <div>
+      <div className='categories-pc'>
         {
           categories.map((category, i) => {
             return (
@@ -13,7 +31,22 @@ const Categories = () => {
             )
           })
         }
+      </div>
+
+      <div className="categories-mobile">
+        <button className="dropdown-button" onClick={handleClick}>{dropdownSymbol}</button>
+        <div className="categories" style={dropdownMenu}>
+          {
+            categories.map((category, i) => {
+              return (
+                <Category key={i} category={category} />
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
+
   );
 }
 
