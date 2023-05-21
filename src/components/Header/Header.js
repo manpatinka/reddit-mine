cd923fbf0f4cc8c64127a81729f64f53d7d89220
Evgenia <evgeniayurkova@gmail.com>
2023-05-21 12:04:30 +0300

    add functionality to logo button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import './header.css';
 import Search from '../Search/Search';
+import { useDispatch } from 'react-redux';
+import { setSelectedSubreddit } from '../../features/FeedSlice';
 
 
 const Header = () => {
-    return ( 
-        <div id="header">
-          <div id="header-left">
-            <img src="logo.png" alt="logo" width="50" height="50" />
-            <p id="title">Not Actually Reddit</p>
-          </div>
-          <div id="header-right">
-            <Search />
-          </div>
-        </div>
-     );
+  const dispatch = useDispatch();
+
+  const handleLogoClick = () => {
+    dispatch(setSelectedSubreddit('r/popular'));
+  }
+
+  return (
+    <div id="header">
+      <div id="header-left">
+        <button id="logo-button" onClick={handleLogoClick}>
+          <img src="logo.png" alt="logo" width="50" height="50" />
+        </button>
+        <p id="title">Not Actually Reddit</p>
+      </div>
+      <div id="header-right">
+        <Search />
+      </div>
+    </div>
+  );
 }
- 
+
 export default Header;
